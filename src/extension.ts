e7d7e212a8327524f5630a16b1cb1577e5b91787
Martin Bošanský <martin.bosansky@quest.com>
2022-06-29 17:40:36 +0200

    Added Files and ModuleResolver

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, workspace } from 'vscode';
+import { LoggingService } from './loggingService';
 import { getConfig } from './util';
+import { EXTENSION_DISABLED, RESTART_TO_ENABLE } from './message';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const extensionName = process.env.EXTENSION_NAME;
+const extensionVersion = process.env.EXTENSION_VERSION;
+
+export function activate(context: ExtensionContext) {
+	const loggingService = new LoggingService();
 	const { enable } = getConfig();
 
+	loggingService.logInfo(`Extension Name: ${extensionName}.`);
+	loggingService.logInfo(`Extension Version: ${extensionVersion}`);
+
 	if (!enable) {
+		loggingService.logInfo(EXTENSION_DISABLED);
+		context.subscriptions.push(
+			workspace.onDidChangeConfiguration(event => {
+				if (event.affectsConfiguration('prettier-more.enable')) {
+					loggingService.logWarning(RESTART_TO_ENABLE);
+				}
+			})
+		);
+
+		return;
 	}
+
+	const moduleResolver = new ModuleResolver(loggingService);
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json

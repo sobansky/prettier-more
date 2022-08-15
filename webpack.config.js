7d550617d3b6ba559cce2a42717637bac01c0cc1
Martin Bošanský <martin.bosansky@quest.com>
2022-08-15 19:40:02 +0200

    Advanced webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const extensionPackage = require('./package.json');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
-  target: 'webworker',
+  target: 'node',
   entry: './src/extension.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -65,4 +65,63 @@ const config = {
     ],
   },
 };
-module.exports = config;
+
+const browserConfig = /** @type WebpackConfig */ {
+  mode: "none",
+  target: "webworker", // web extensions run in a webworker context
+  entry: {
+    "web-extension": "./src/extension.ts",
+  },
+  output: {
+    filename: "[name].js",
+    // eslint-disable-next-line no-undef
+    path: path.join(__dirname, "./dist"),
+    libraryTarget: "commonjs",
+  },
+  resolve: {
+    mainFields: ["module", "main"],
+    extensions: [".ts", ".js", ".mjs"], 
+    alias: {      
+      "./ModuleResolver": "./BrowserModuleResolver",
+    },
+    fallback: {
+      // eslint-disable-next-line no-undef
+      path: require.resolve("path-browserify"),
+      process: require.resolve('process/browser'),
+      // eslint-disable-next-line no-undef
+      util: require.resolve("util/"),
+      os: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  externals: {
+    vscode: "commonjs vscode", // ignored because it doesn't exist
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: "source-map",
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      babel: "prettier/esm/parser-babel.mjs",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify({}),
+      "process.env.BROWSER_ENV": JSON.stringify("true"),
+    }),
+  ],
+};
+module.exports = [config, browserConfig];
